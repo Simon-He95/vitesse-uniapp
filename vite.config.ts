05b30b36d55eeee3cdcea964fdd174b6a7692739
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-18 01:09:15 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import AutoImport from 'unplugin-auto-import/vite'
 import { vitePluginTailwindUni } from 'vite-plugin-tailwind-uni'
-import fs from 'fs'
-
-const args = process.argv.slice(2);
-const i = args.findIndex(item => item === '-p')
-const filename = i === -1 ? 'h5' : args[i + 1]
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,25 +16,6 @@ export default defineConfig({
     AutoImport({
       imports: ['vue'],
       dts: true,
-    }),
-    {
-      enforce: 'post',
-      buildEnd() {
-        const files = fs.readdirSync("static", 'utf-8')
-        setTimeout(async () => {
-          const desc = `./dist/build/${filename}/static`
-          if (!fs.existsSync(desc)) {
-            await fs.mkdirSync(desc)
-          }
-          files.forEach((file: string) => {
-            fs.copyFileSync(`static/${file}`, `${desc}/${file}`);
-          })
-        },1000)
-
-      }
-    }
-
+    })
   ],
 });
-
-
